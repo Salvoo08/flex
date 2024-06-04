8f250775e27640bb413484b3cd4fd6e2f8756548
Salvoo08 <165826507+Salvoo08@users.noreply.github.com>
2024-06-04 17:24:17 +0200

    farlo vedere al tutor

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ lista_fatti=["Elon Musk sostiene che i social network sono progettati per tenerc
             "Secondo uno studio condotto nel 2018, più del 50'%' delle persone di età compresa tra i 18 e i 34 anni si considera dipendente dal proprio smartphone.", 
             "I social network hanno aspetti positivi e negativi e dobbiamo essere consapevoli di entrambi quando usiamo queste piattaforme.", 
             "Lo studio della dipendenza tecnologica è una delle aree più rilevanti della ricerca scientifica moderna."]
+lista_rischi=["UN problema di avere una dipendenza dai social newtork è la progressiva perdita di capacità o mancata creazione di competenze sociali, emozionali e relazional",
+              "Il troppo utilizzodei degli strumenti tecnologici può alterale il ritmo del sonno",
+              "Troppo tempo passato ha giocare hai videogichi puo farti credere di essere invicibile è cio può portarti ha mettere in pericolo la tua persona o altre persone",
+              " il troppo utilizzo dei social newtork può cuasre nei giovani la comparsa di disturbi alimentari"]
 
 @app.route("/")
 def ciao_mondo():
-    #return f'<p>{random.choice(lista_fatti)}</p>'
-    return '<h1>Ciao questa è la home pagi trami il link puoi scoprire un fatto sulle dipendenze</h1> <a href ="/fatto_causuale">vedi dei fatti causuali</a>'
+    return '<h1>Ciao questa è la home pagi trami il link puoi scoprire un fatto sulle dipendenze</h1> <a href ="/fatto_causuale">vedi dei fatti causuali</a> <a href ="/risci_per_chi_ha_una_dipendenza_dalla_tecnologia"> vedi i problemi di avere una dipendenza dalla tecnologia</a>'
 @app.route("/fatto_causuale")
 def fatti():
     return f'<p>{random.choice(lista_fatti)}</p>'
+@app.route("/risci_per_chi_ha_una_dipendenza_dalla_tecnologia")
+def rischi():
+    return f'<p>{random.choice(lista_rischi)}</p>
+
 app.run(debug=True)
